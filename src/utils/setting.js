5593e49096e6ac76d602000b45e486356b1cd13a
tkp1123 <907714175@qq.com>
2021-05-28 12:10:09 +0800

    pro 接口更改

--- src/utils/setting.js
+++ src/utils/setting.js
@@ -1,7 +1,7 @@
 const setting = {
     baseUrl: {
         dev: 'http://192.168.2.128:12151/api',
-        pro: 'http://localhost:3000'
+        pro: 'http://192.168.2.128:12151/api'
     },
 }
 export default setting
